0e56bb405656f8628685d6ad8d296e147a452cb0
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-10-24 01:07:13 +0330

    add styles to TodoList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,10 +1,14 @@
 import Todo from "../Todo/Todo";
-
+import styles from "./TodoList.module.css";
 
 const TodoList = ({ todos }) => {
-  return todos.map((todo) => {
-    return <Todo key={todo.id} todo={todo}/>;
-  });
+  return (
+    <section className={styles.todoList}>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo} />;
+      })}
+    </section>
+  );
 };
 
 export default TodoList;
