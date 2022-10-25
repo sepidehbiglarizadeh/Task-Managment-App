9fa63d5538d8e4bfc067a0de83e9c512b226cf35
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-10-25 12:42:42 +0330

    some styles changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,6 @@ import { FaTasks,FaSearch,FaCheck ,FaHourglassStart} from "react-icons/fa";
 const Navbar = ({onFilter}) => {
   return (
     <nav className={styles.nav}>
-      <button ><FaSearch/></button>
       <button onClick={()=>onFilter("all")}><FaTasks/></button>
       <button onClick={()=>onFilter("completed")}><FaCheck/></button>
       <button onClick={()=>onFilter("unCompleted")}><FaHourglassStart/></button>
